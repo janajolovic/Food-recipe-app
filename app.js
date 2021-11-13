9b81de95149ace7b779710091fe436499ef7b317
Jana Jolovic <janajolovic04@gmail.com>
2021-11-13 23:03:00 +0100

    fetching meal recipes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const recipeCloseBtn = document.querySelector(".close-btn");
 
 searchBtn.addEventListener("click", getMealList);
 
+mealList.addEventListener("click", getMealRecipe);
+
 
 function getMealList() {
     let searchInputText = document.querySelector(".search-bar").value.trim();
@@ -15,9 +17,6 @@ function getMealList() {
             if (data.meals) {
                 data.meals.forEach(meal => {
                     html += `
-                        
-
-
                         <div class="meal-item" data-id = ${meal.idMeal}>
                             <div class="meal-img">
                                 <img src="${meal.strMealThumb}">
@@ -27,11 +26,51 @@ function getMealList() {
                                 <a class="recipe-btn" href="">Get recipe</a>
                             </div>
                         </div>
-
-
                     `
+
                 })
             }
             mealList.innerHTML = html;
+
+
         })
+    }
+
+
+function getMealRecipe(e) {
+    e.preventDefault();
+    if (e.target.classList.contains("recipe-btn")) {
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`) 
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                let html = ""
+
+                if (data.meals) {
+                        html += `
+                            <button type="button" class='close-btn'><i class="fas fa-times"></i></button>
+                            <div class="meal-details-content">
+                                <h2 class="meal-title">${data.meals[0].strMeal}</h2>
+                                <p class="meal-category">${data.meals[0].strCategory}</p>
+                                <div class="meal-instructions">
+                                    <h3>Instructions:</h3>
+                                    <p>${data.meals[0].strInstructions}</p>
+                                    <div class="recipe-meal-img">
+                                        <img src='${data.meals[0].strMealThumb}'>
+                                    </div>
+                                    <div class="meal-link">
+                                        <a href="${data.meals[0].strYoutube}" target="_blank">Watch video</a>
+                                    </div>
+                                </div>
+                            </div>
+                        `
+                }
+
+                console.log(html)
+                let MealDetails = document.querySelector(".meal-details")
+                MealDetails.style.display = "block";
+                MealDetails.innerHTML = html;
+            })
+    }
 }
